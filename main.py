1d131f1014141a7094d4bed7f18a44c64158b598
Ryan Harris <ryan@synthesis.co.za>
2024-08-16 13:24:50 +0200

    going through tutorial...

--- main.py
+++ main.py
@@ -20,6 +20,36 @@ def add_user_input():
     entry.pack()
 
 
-add_user_input()
+def add_frame():
+    frame_a = tk.Frame()
+    frame_b = tk.Frame()
+
+    label_a = tk.Label(master=frame_a, text="Frame a")
+    label_a.pack()
+
+    label_b = tk.Label(master=frame_b, text="Frame b")
+    label_b.pack()
+
+    frame_a.pack()
+    frame_b.pack()
+
+
+def add_frame_with_reliefs():
+    border_effects = {
+        "flat": tk.FLAT,
+        "sunken": tk.SUNKEN,
+        "raised": tk.RAISED,
+        "groove": tk.GROOVE,
+        "ridge": tk.RIDGE,
+    }
+
+    for relief_name, relief in border_effects.items():
+        frame = tk.Frame(master=window, relief=relief, borderwidth=5)
+        frame.pack(side=tk.LEFT)
+        label = tk.Label(master=frame, text=relief_name)
+        label.pack()
+
+
+add_frame_with_reliefs()
 
 window.mainloop()
