31c314097f11293ba44b75dbedd29e54f8169580
Ryan Harris <ryan@synthesis.co.za>
2024-08-23 18:25:43 +0200

    going through tutorial...

--- main.py
+++ main.py
@@ -171,6 +171,28 @@ def grid_example_4():
     label4.grid(row=0, column=3, sticky=StickyGridPosition.cell_fill())
 
 
-grid_example_4()
+def events():
+    window.rowconfigure(0, minsize=50, weight=1)
+    window.columnconfigure([0, 1, 2], minsize=50, weight=1)
+
+    def decrease():
+        value = int(lbl_value["text"])
+        lbl_value["text"] = f"{value - 1}"
+
+    btn_decrease = tk.Button(master=window, text="-", command=decrease)
+    btn_decrease.grid(row=0, column=0, sticky=StickyGridPosition.cell_fill())
+
+    lbl_value = tk.Label(master=window, text="0")
+    lbl_value.grid(row=0, column=1)
+
+    def increase():
+        value = int(lbl_value["text"])
+        lbl_value["text"] = f"{value + 1}"
+
+    btn_increase = tk.Button(master=window, text="+", command=increase)
+    btn_increase.grid(row=0, column=2, sticky=StickyGridPosition.cell_fill())
+
+
+events()
 
 window.mainloop()
